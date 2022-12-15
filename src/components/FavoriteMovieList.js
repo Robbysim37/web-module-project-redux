d0a702046d140a5891da715d6d5d04d004d2cf04
Robby Lewis <Robbysim37@gmail.com>
2022-12-15 15:21:29 -0500

    finished S10-M2

--- src/components/FavoriteMovieList.js
+++ src/components/FavoriteMovieList.js
@@ -1,19 +1,24 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import {removeFavorite} from "../actions/favoritesActions"
 
 
 const FavoriteMovieList = (props) => {
     const favorites = [];
+
+    const removeFavoriteClickHandler = (e) => {
+        props.removeFavorite(parseInt(e.target.id))
+    }
     
     return (<div className="col-xs savedContainer">
         <h5>Favorite Movies</h5>
         {
-            favorites.map(movie=>{
+            props.displayFavorites && props.favorites.map(movie=>{
                 return <div key={movie.id}>
                     <Link className="btn btn-light savedButton" to={`/movies/${movie.id}`}>
                         {movie.title}
-                        <span><span class="material-icons">remove_circle</span></span>
+                        <span><span class="material-icons" id={movie.id} onClick={removeFavoriteClickHandler}>remove_circle</span></span>
                     </Link> 
                 </div>
             })
@@ -21,5 +26,9 @@ const FavoriteMovieList = (props) => {
     </div>);
 }
 
+const mapStateToProps = state => ({
+    favorites: state.favoritesReducer.favorites,
+    displayFavorites:state.favoritesReducer.displayFavorites
+})
 
-export default FavoriteMovieList;
+export default connect(mapStateToProps,{removeFavorite})(FavoriteMovieList);
